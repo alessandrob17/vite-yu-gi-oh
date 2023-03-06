957ae241f0367fb9938d035de5e2cc3ba76c37fe
alessandrob17 <a.baldipardi@gmail.com>
2023-03-06 15:18:51 +0100

    FEAT vue

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+      <header>
+        <h1>Yu-Gi-Oh Cards</h1>
+      </header>
+      <main>
+      
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  }
+  </script>
+  
+  <style>
+  header {
+    background-color: #0080ff;
+    color: #fff;
+    text-align: center;
+    padding: 1rem;
+  }
+  
+  main {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  </style>
+  
